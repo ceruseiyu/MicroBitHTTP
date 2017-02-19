58b9d8b5bea134fc7c17f8cd73e1fd8af0921bd7
Ceru <eurocracy2@gmail.com>
2017-02-19 21:54:22 +0000

    First commit

---
+++ yotta_modules/microbit-dal/inc/bluetooth/MicroBitHTTPService.h
@@ -0,0 +1,25 @@
+#ifndef MICROBIT_HTTP_SERVICE_H
+#define MICROBIT_HTTP_SERVICE_H
+
+
+#include "MicroBitConfig.h"
+#include "ble/UUID.h"
+#include "ble/BLE.h"
+
+extern const uint8_t MicroBitHTTPServiceUUID[];
+extern const uint8_t MicroBitHTTPServiceUrlUUID[];
+
+class MicroBitHTTPService {
+  public:
+    MicroBitHTTPService(BLEDevice &_ble);
+    void onDataWritten(const GattWriteCallbackParams * params);
+    void updateURL(char* url);
+  private:
+    BLEDevice &ble;
+
+    char* urlCharacteristicBuffer;
+
+    GattAttribute::Handle_t urlCharacteristicHandle;
+};
+
+#endif
---
+++ yotta_modules/microbit-dal/source/bluetooth/MicroBitHTTPService.cpp
@@ -0,0 +1,36 @@
+#include "MicroBitConfig.h"
+#include "ble/UUID.h"
+
+#include "MicroBitHTTPService.h"
+
+MicroBitHTTPService::MicroBitHTTPService(BLEDevice &_ble) : ble(_ble) {
+  GattCharacteristic urlCharacteristic(MicroBitHTTPServiceUrlUUID, (uint8_t*)&urlCharacteristicBuffer, 1, 20, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE);
+  GattCharacteristic *characteristics[] = {&urlCharacteristic};
+
+  urlCharacteristicBuffer = (char*)malloc(20);
+
+  urlCharacteristic.requireSecurity(SecurityManager::MICROBIT_BLE_SECURITY_LEVEL);
+
+  GattService service(MicroBitHTTPServiceUUID, characteristics, sizeof(characteristics) / sizeof(GattCharacteristic*));
+  ble.addService(service);
+
+  urlCharacteristicHandle = urlCharacteristic.getValueHandle();
+
+  ble.onDataWritten(this, &MicroBitHTTPService::onDataWritten);
+
+}
+
+void MicroBitHTTPService::onDataWritten(const GattWriteCallbackParams *params) {
+
+}
+
+void MicroBitHTTPService::updateURL(char* url) {
+  ble.gattServer().write(urlCharacteristicHandle, (uint8_t*)&urlCharacteristicBuffer, sizeof(urlCharacteristicBuffer));
+}
+const uint8_t MicroBitHTTPServiceUUID[] = {
+  0x13, 0x51, 0x63, 0x4a, 0x09, 0xd1, 0x48, 0x46, 0x99, 0xb9, 0xee, 0x31, 0x12, 0xc3, 0xf5, 0x5b
+};
+
+const uint8_t MicroBitHTTPServiceUrlUUID[] = {
+  0x13, 0x51, 0x3f, 0x02, 0x09, 0xd1, 0x48, 0x46, 0x99, 0xb9, 0xee, 0x31, 0x12, 0xc3, 0xf5, 0x5b
+};
