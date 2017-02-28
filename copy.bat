4076ceb03fc4be2143d7b791ca96131f5bc306f6
Ceru <eurocracy2@gmail.com>
2017-02-28 01:43:26 +0000

    Implementing events

--- copy.bat
+++ copy.bat
@@ -1,6 +1,7 @@
 copy source\main.cpp C:\Users\whati\Desktop\http\microbit-samples-master\source
 copy yotta_modules\microbit-dal\inc\bluetooth\MicroBitBLEManager.h C:\Users\whati\Desktop\http\microbit-samples-master\yotta_modules\microbit-dal\inc\bluetooth
 copy yotta_modules\microbit-dal\inc\bluetooth\MicroBitHTTPService.h C:\Users\whati\Desktop\http\microbit-samples-master\yotta_modules\microbit-dal\inc\bluetooth
+copy yotta_modules\microbit-dal\inc\bluetooth\ExternalEvents.h C:\Users\whati\Desktop\http\microbit-samples-master\yotta_modules\microbit-dal\inc\bluetooth
 copy yotta_modules\microbit-dal\inc\core\MicroBitConfig.h C:\Users\whati\Desktop\http\microbit-samples-master\yotta_modules\microbit-dal\inc\core
 copy yotta_modules\microbit-dal\source\bluetooth\MicroBitHTTPService.cpp C:\Users\whati\Desktop\http\microbit-samples-master\yotta_modules\microbit-dal\source\bluetooth
 pause
